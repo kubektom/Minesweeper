f17e7e6f9f296f4c75c7f318321c7268144eb22a
Kuba\Kuba <jakub.tomczyk01@gmail.com>
2018-04-06 22:35:33 +0200

    Base is done. Bugs are fixed. TO DO: Add settings screen

--- src/main/java/sample/Controllers/MainController.java
+++ src/main/java/sample/Controllers/MainController.java
@@ -1,19 +1,17 @@
 package sample.Controllers;
 
 import javafx.application.Platform;
-import javafx.event.EventHandler;
+import javafx.beans.property.IntegerProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonBar;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
-import javafx.scene.input.ContextMenuEvent;
+import javafx.scene.input.MouseButton;
 import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
-import javafx.stage.Window;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -22,40 +20,61 @@ import java.util.Optional;
 public class MainController {
 
     @FXML
-    BorderPane mainPane; //uploading panel from FXML
+    BorderPane mainPane;
 
+    @FXML
+    Label flagLabel;
 
-    private Tile[][] grid = new Tile[20][20];
+    @FXML
+    Label bombLabel;
+
+    public static final int SIZE_OF_FIELD = 20;
+
+    private Tile[][] grid = new Tile[SIZE_OF_FIELD][SIZE_OF_FIELD];
+    private ArrayList<Tile> bombList= new ArrayList<>();
+    private ArrayList<Tile> nonBombList= new ArrayList<>();
+    private IntegerProperty numberOfBombs=new SimpleIntegerProperty(0);
+    private IntegerProperty numberOfFlags=new SimpleIntegerProperty(0);
 
     public MainController() {
-        System.out.println("działa");
     }
 
     @FXML
     void initialize() {
+        bombLabel.textProperty().bind(numberOfBombs.asString());
+        flagLabel.textProperty().bind(numberOfFlags.asString());
         mainPane.setCenter(createField());
     }
 
-    Pane createField() {
-        Pane mainPanel = new Pane();
-        for (int x = 0; x < 20; x++) {
-            for (int y = 0; y < 20; y++) {
+    private Pane createField() {
 
-                Tile tile = new Tile(x, y, Math.random() < 0.2);
+        Pane mainPanel = new Pane();
+
+        for (int x = 0; x < SIZE_OF_FIELD; x++) {
+            for (int y = 0; y < SIZE_OF_FIELD; y++) {
+
+                Tile tile = new Tile(x, y, Math.random() < 0.1);
+                if (tile.isBomb) {
+                    bombList.add(tile);
+                }else{
+                    nonBombList.add(tile);
+                }
                 grid[x][y] = tile;
                 mainPanel.getChildren().add(tile);
 
             }
         }
-        for (int x = 0; x < 20; x++) {
-            for (int y = 0; y < 20; y++) {
+        numberOfBombs.set(bombList.size());
+        numberOfFlags.set(bombList.size());
+        for (int x = 0; x < SIZE_OF_FIELD; x++) {
+            for (int y = 0; y < SIZE_OF_FIELD; y++) {
 
                 int howManyBombsAround = 0;
                 List<Tile> neighbors = getNeighbors(grid[x][y]);
                 for (int i = 0; i < neighbors.size(); i++) {
                     if (!grid[x][y].textLabel.getText().equals("")) {
                         if (neighbors.get(i).textLabel.getText().equals(""))
-                            neighbors.get(i).click();
+                            neighbors.get(i).click(MouseButton.PRIMARY);
                     }
 
                     if (neighbors.get(i).isBomb) {
@@ -67,7 +86,6 @@ public class MainController {
                     grid[x][y].textLabel.setText("" + howManyBombsAround);
 
                 if (grid[x][y].isBomb) {
-
                     grid[x][y].textLabel.setText("X");
                 }
             }
@@ -97,8 +115,8 @@ public class MainController {
             int newX = tile.x + dx;
             int newY = tile.y + dy;
 
-            if (newX >= 0 && newX < 20
-                    && newY >= 0 && newY < 20) {
+            if (newX >= 0 && newX < SIZE_OF_FIELD
+                    && newY >= 0 && newY < SIZE_OF_FIELD) {
                 neighbors.add(grid[newX][newY]);
             }
         }
@@ -111,68 +129,122 @@ public class MainController {
         private int x, y;
         private boolean isClicked = false;
         Label textLabel = new Label("");
-        private boolean isBomb = false;
+        Label flagLabel = new Label("F");
+        private boolean isBomb=false;
+        private boolean isFlag=false;
         Rectangle border = new Rectangle(30, 30);
 
-        public Tile(int x, int y, boolean isBomb) {
+        Tile(int x, int y, boolean isBomb) {
             this.x = x;
             this.y = y;
             textLabel.setWrapText(true);
             textLabel.setFont(Font.font(25));
             textLabel.setVisible(false);
+            flagLabel.setWrapText(true);
+            flagLabel.setFont(Font.font(25));
+            flagLabel.setVisible(false);
             border.setFill(Color.CHOCOLATE);
             border.setStroke(Color.BISQUE);
             this.setPrefWidth(30);
             this.setPrefHeight(30);
             this.getChildren().add(border);
             this.getChildren().add(textLabel);
+            this.getChildren().add(flagLabel);
             this.isBomb = isBomb;
             this.setTranslateX(x * 30 + 2);
             this.setTranslateY(y * 30 - 2);
-            this.setOnMouseClicked(event -> click());
+            this.setOnMouseClicked(event -> click(event.getButton()));
         }
 
-        public void click() {
-            if (isClicked) {
-                return;
+
+        private void click(MouseButton mouseButton) {
+            if(mouseButton.equals(MouseButton.PRIMARY)) {
+                if (isClicked || isFlag) {
+                    return;
+                }
+                isClicked=true;
+                textLabel.setVisible(true);
+                border.setFill(Color.AZURE);
+                border.setStroke(Color.BISQUE);
+                if (isBomb) {
+
+                    for (int i=0; i<bombList.size();i++){
+                        bombList.get(i).isClicked=true;
+                        bombList.get(i).border.setFill(Color.RED);
+                        bombList.get(i).border.setStroke(Color.DARKRED);
+                        bombList.get(i).textLabel.setVisible(true);
+                        bombList.get(i).flagLabel.setVisible(false);
+                    }
+                    sendAlert("Lost");
+                }
+                if (textLabel.getText().equals("")) {
+                    for (Tile tile : getNeighbors(this)) {
+                        tile.click(MouseButton.PRIMARY);
+                    }
+                }
             }
+            if(mouseButton.equals(MouseButton.SECONDARY)) {
+                flag();
+            }
+            checkWin();
+        }
 
-            isClicked = true;
-            textLabel.setVisible(true);
-            border.setFill(Color.AZURE);
-            border.setStroke(Color.BISQUE);
-            if(isBomb){
-                border.setFill(Color.RED);
-                border.setStroke(Color.DARKRED);
-                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
-                alert.setTitle("You lost!");
-                alert.setHeaderText("You lost the game. ");
-                alert.setContentText("Do you want to try again?");
 
-                Optional<ButtonType> result = alert.showAndWait();
-                if (result.get() == ButtonType.OK){
-                    mainPane.setCenter(createField());
-                    return;
-                } else {
-                    Platform.exit();
-                    return;
+        private void flag(){
+            if(!isClicked){
+                if(!isFlag && numberOfFlags.get() >0){
+                    flagLabel.setVisible(true);
+                    isFlag=true;
+                    numberOfFlags.set(numberOfFlags.get()-1);
+                }
+                else{
+                    flagLabel.setVisible(false);
+                    isFlag=false;
+                    numberOfFlags.set(numberOfFlags.get()+1);
+                }
+
+
+            }
+        }
+
+        private void checkWin(){
+            boolean flagsOK1=true;
+            boolean flagsOK2=true;
+
+            for(int i=0; i<nonBombList.size();i++){
+                if(!nonBombList.get(i).isClicked){
+                    flagsOK1=false;
                 }
 
             }
-            if (textLabel.getText().equals("")) {
-                getNeighbors(this).forEach(Tile::click);
+
+            for(int i=0; i<bombList.size();i++){
+                if(!bombList.get(i).isFlag){
+                    flagsOK2=false;
+                }
             }
+
+            if(flagsOK1 || flagsOK2){
+                sendAlert("WON");
+            }
+
+
         }
 
-        public void flag() {
-            if (isClicked) {
-                return;
+        private void sendAlert(String result){
+            Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+            alert.setTitle("You "+result+"!");
+            alert.setHeaderText("You "+result+ " the game. ");
+            alert.setContentText("Do you want to try again?");
+            Optional<ButtonType> decision = alert.showAndWait();
+            if (decision.get() == ButtonType.OK) {
+                nonBombList.clear();
+                bombList.clear();
+                mainPane.setCenter(createField());
+            } else {
+                Platform.exit();
             }
-            this.textLabel.setText("F");
-            this.textLabel.setVisible(true);
-            return;
         }
-
 
     }
 }
--- src/main/java/sample/Main.java
+++ src/main/java/sample/Main.java
@@ -5,8 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import sample.Controllers.MainController;
 
 public class Main extends Application {
+    private static final int NUMBER_X_TILES=MainController.SIZE_OF_FIELD;
+    private static final int NUMBER_Y_TILES=MainController.SIZE_OF_FIELD;
+    private static final int WIDTH=NUMBER_X_TILES*30+150;
+    private static final int HEIGHT=NUMBER_Y_TILES*30+10;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,13 +19,13 @@ public class Main extends Application {
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(this.getClass().getResource("/FXML/MainWindow.fxml"));
         BorderPane borderPane = loader.load();
-        borderPane.setPrefHeight(700);
-        borderPane.setPrefWidth(700);
-        BorderPane panel =new BorderPane();
+        borderPane.setPrefHeight(HEIGHT);
+        borderPane.setPrefWidth(WIDTH);
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Minesweeper");
         primaryStage.setScene(scene);
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
